7c8e5807bd783ab04cdaa105e8a5a49ec5a92d3c
Berke <69055194+Fsb3rKe@users.noreply.github.com>
2022-04-14 15:26:23 +0300

    Add files via upload

---
+++ src/gcwingo.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/aiocat/cystem"
+)
+
+type Integrations struct {
+	CommandsFileName string
+}
+
+func main() {
+
+	cliCommand := os.Args[1]
+	commandName := os.Args[2]
+
+	integrationsFileContentBytes, err := ioutil.ReadFile("gcwinGO.integrations.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var integrations Integrations
+	json.Unmarshal(integrationsFileContentBytes, &integrations)
+
+	byt, err := ioutil.ReadFile(integrations.CommandsFileName)
+	var dat map[string]interface{}
+	if err := json.Unmarshal(byt, &dat); err != nil {
+		panic(err)
+	}
+
+	if cliCommand == "use" {
+		for key, value := range dat {
+			if key == commandName {
+				for _, v := range value.([]interface{}) {
+					cystem.RunString(v.(string))
+				}
+			}
+		}
+	}
+
+	if cliCommand == "show" {
+		if commandName == "all" {
+			for key, value := range dat {
+				fmt.Println(key)
+				for _, v := range value.([]interface{}) {
+					fmt.Println(v)
+				}
+				fmt.Println("")
+			}
+		} else {
+			for key, value := range dat {
+				if key == commandName {
+					for _, v := range value.([]interface{}) {
+						fmt.Println(v)
+					}
+				}
+			}
+		}
+	}
+
+	if cliCommand == "edit" {
+		cystem.RunString("notepad " + integrations.CommandsFileName)
+	}
+
+	if cliCommand == "help" {
+		fmt.Printf("\nuse  -> starts the command\nshow -> shows command inside\nedit -> opens the notepad\n")
+	}
+}
---
+++ src/go.mod
@@ -0,0 +1,5 @@
+module gcwingo.go
+
+go 1.18
+
+require github.com/aiocat/cystem v0.0.0-20220414114118-66790cb7c3d6 // indirect
